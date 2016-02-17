5581cde280f2d814a97b8d8d695a5c502c0aba11
Robert Lara <lara.robert9@gmail.com>
2016-02-17 16:02:42 +0100

    Plantilla pels records del usuari

---
+++ application/views/game/box_level_user.php
@@ -0,0 +1,37 @@
+<!-- Block  -->
+<div class="col-lg-3 col-md-4">
+    <div class="panel panel-yellow" data-level="<?php echo $i+1;?>">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-12 text-right">
+                    <div class="huge"><i class="fa fa-star"></i><?php echo $i + 1; ?><i class="fa fa-star"></i></div>
+                    <div>
+                        <span>
+                        <?php
+                        if ($getUserRankingTime[$i]->time == null) {
+                            echo "No definit";
+                        } else {
+                            echo $getUserRankingTime[$i]->time;
+                        }
+                        ?>
+                        </span>
+                        <i class="fa fa-clock-o"></i>
+                    </div>
+                    <div>
+                        <span>
+                        <?php
+                        if ($getUserRankingMoves[$i]->moves == 0){
+                            echo "No definit";
+                        }else{
+                            echo $getUserRankingMoves[$i]->moves;
+                        }
+                        ?>
+                        </span>
+                        <i class="fa fa-hand-pointer-o"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>

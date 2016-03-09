6e9d8799e759c67a8accf8b1082b31dfb75a4f55
Robert Lara <lara.robert9@gmail.com>
2016-03-09 21:17:35 +0100

    Comentarias JS (dashboard)

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,15 +1,19 @@
 $(function () {
+    //Obre el modal per crear nivell
     $("#openMakeBoard").click(function () {
         makeBoard();
     });
 });
 
+//Crear un tauler buit per indicar posicions
 function makeBoard(){
     $('#makeBoard .modal-body').html('<div id="board" class="container-fluid text-center"></div>');
     var board = $('#makeBoard #board');
+    //Crear cinc files
     for (var i = 0; i < 5; i++) {
         $(board).append('<div class="row" data-row="' + i + '"></div>');
     }
+    //Afegeix les posicions
     var rows = $(board).find('.row');
     $(rows).each(function (index, value) {
         for (var i = 0; i < 5; i++) {
@@ -25,23 +29,24 @@ function makeBoard(){
     $(board).append('<button id="btnSave" type="button" onclick="saveLevel()" class="btn btn-success">Guardar nivell</button>');
 }
 
+//Guarda el nou nivell creat
 function saveLevel() {
-    $('#makeBoard').modal('hide');
+    $('#makeBoard').modal('hide');  //Tanca el modal
     var structure = "";
     var position = $('#makeBoard .position');
+    //Obté l'estructura del tauler
     position.each(function(){
-        if($(this).hasClass('active')){
+        if($(this).hasClass('active')){ //Segons la classe rep 1 o 0
             structure += "1";
         }else{
             structure += "0";
         }
     });
     $.ajax({
-        url: '/Codeigniter/game/saveLevel/' + structure,
+        url: '/Codeigniter/game/saveLevel/' + structure,    //recurs
         type: 'post',
         dataType: 'json',
         success: function (response) {
-            console.log(response);
         }
     });
 
